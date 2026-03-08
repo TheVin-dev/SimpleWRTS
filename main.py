a8821e830851f04c02f3af52d4fd63cb809682fb
Vincent <vincent_denronden@live.nl>
2026-03-08 17:13:24 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import os 
+import csv 
+import pathlib 
+import numpy as np
+from nicegui import ui 
+
+
+DATA_PATH = pathlib.Path(r"M:\1. Personal\Programming\simpleWRTS\data")
+TOTAL_COLUMN = 2 
+CORRECT_COLUMN = 3 
+def load_list(path: pathlib.Path):
+    if not os.path.exists(path):
+        raise KeyError
+    paths = os.listdir(path)
+    paths = [os.path.join(DATA_PATH,paths[i]) for i in range(len(paths))]
+    with open(paths[0]) as file:
+        reader = csv.reader(file, delimiter=',', quotechar='|')
+        arr = []
+        for row in reader:
+            arr.append(row)
+    return np.array(arr)
+
+def evaluate(target,guess):
+    
+    if "".join(target.split())  == "".join(guess.split()):
+        return True 
+    return False
+
+def try_again(arr, idx, depth):
+    print("You have three more guesses")
+    while depth < 3:
+        choice = arr[idx][0]
+        print(f"Italian: {choice} ----- for testing: {arr[idx][1]}")
+        s = str(input("Your guess: "))
+        res = evaluate(arr[idx][1],s)
+        arr[idx,TOTAL_COLUMN] =  int(arr[idx,TOTAL_COLUMN])+ 1 
+        if res: 
+            arr[idx,CORRECT_COLUMN] = int(arr[idx,CORRECT_COLUMN]) + 1 
+
+            return res
+        depth += 1 
+    return res 
+
+
+def main_loop():
+    arr = load_list(DATA_PATH)
+    arr = np.concatenate((arr,np.zeros(shape=arr.shape).astype(int)),axis=1)  # Nx4 arr, 3 column is total, 4 column is correct 
+    PLAYING = True
+    print("Welcome to my SimpleWRTS\nA simple tool to learn words (hopefully)")
+    print("Translate the following words to the other language:")
+    while PLAYING:     
+        choic_idx = np.random.choice(arr[1:].shape[0])
+        choice = arr[choic_idx][0]
+        print(f"Italian: {choice} ----- for testing: {arr[choic_idx][1]}")
+        s = str(input("Your guess: "))
+        res = evaluate(arr[choic_idx][1],s)
+        if res:
+            arr[choic_idx,TOTAL_COLUMN]   = int(arr[choic_idx,TOTAL_COLUMN])  + 1 
+            arr[choic_idx,CORRECT_COLUMN] = int(arr[choic_idx,CORRECT_COLUMN])+ 1 
+
+            continue 
+        else:
+            res = try_again(arr, choic_idx, depth=0)
+            print('On a list maybe?')
+if __name__ == "__main__":
+    main_loop()
